aa67e124a476c61650da95a8250e71a131cc5a86
pwnflakes <pwnflakes25@gmail.com>
2019-07-09 17:29:03 +0300

    Implemented getPost from firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PostComponent} from './post/post.component';
+import {ItemComponent} from './item/item.component';
+import {PostMakerComponent} from "./post/post-maker/post-maker.component";
+import {SignupComponent} from "./auth/signup/signup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path: "",
+component:PostComponent
+},
+{
+  path: 'listing',
+  component: PostComponent
+},
+{
+  path: 'item/:id',
+  component: ItemComponent
+},
+{
+  path: 'postEditor',
+  component: PostMakerComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {MatDialogModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule, FormsModule} from "@angular/forms";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { firebaseConfig } from '../environments/firebase.config';
+import * as firebase from 'firebase/app';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
+import {PostService} from "./post/post.service";
+import { PostListComponent } from './post/post-list/post-list.component';
+import { ItemComponent } from './item/item.component';
+import { PostMakerComponent } from './post/post-maker/post-maker.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import {AuthService} from "./auth/auth.service";
+import {DataStorageService} from "./shared/data-storage.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    PostListComponent,
+    ItemComponent,
+    PostMakerComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule // imports firebase/auth, only needed for auth features
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PostService, AuthService, DataStorageService],
+  bootstrap: [AppComponent],
+  entryComponents: [SignupComponent, LoginComponent]
 })
 export class AppModule { }
